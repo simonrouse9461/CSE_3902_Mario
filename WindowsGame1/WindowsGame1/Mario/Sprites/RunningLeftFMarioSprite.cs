c99dea215394b84590c7fb566490daae913703a1
BOWEN DAI <bowen-dai@outlook.com>
2015-06-04 07:29:04 +0000

    Added 13 files to Sprites

---
+++ WindowsGame1/WindowsGame1/Mario/Sprites/RunningLeftFMarioSprite.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Content;
+
+namespace WindowsGame1
+{
+    public class RunningLeftFMarioSprite : SpriteKernel
+    {
+        protected override void Initialize()
+        {
+            // Source parameters
+            const int totalFrames = 3;
+            Vector2 startCoordinate = new Vector2(95, 120);
+            Vector2 endCoordinate = new Vector2(170, 155);
+
+            // Animation parameters
+            const int period = 3;
+
+            Source = new SpriteSource(startCoordinate, endCoordinate, totalFrames);
+            Animation = new SpriteAnimation(
+                phase =>
+                {
+                    int[] frameSequence = {0,1,2};
+                    return frameSequence[phase];
+                }, 
+                period);
+        }
+
+        public override void Load(ContentManager content)
+        {
+            Source.Load(content, "Mario");
+        }
+    }
+}
