7489b91f668eaee1cd4b9921fe12b9cb2f0ec78f
Chuhan Feng <simonrouse9461@gmail.com>
2015-06-04 22:49:01 -0400

    update

---
+++ WindowsGame1/WindowsGame1/Kernels/MotionStateKernel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace WindowsGame1
+{
+    public abstract class MotionStateKernel : IMotionState
+    {
+        protected Vector2 Location;
+
+        protected Counter Timer;
+
+        protected Dictionary<ObjectMotion, bool> MotionList; 
+
+        protected MotionStateKernel(Vector2 location, int frequency = 10)
+        {
+            Reset(location, frequency);
+        }
+
+        protected abstract void Initialize();
+
+        public Vector2 CurrentLocation()
+        {
+            return Location;
+        }
+
+        public void Reset(Vector2 location, int frequency = 10)
+        {
+            Timer = new Counter(frequency);
+            Location = location;
+            Initialize();
+        }
+        public void Update()
+        {
+            foreach (var motion in MotionList)
+            {
+                if (motion.Value)
+                {
+                    motion.Key.Update();
+                    Location += motion.Key.GetValue();
+                }
+            }
+        }
+    }
+}
---
+++ WindowsGame1/WindowsGame1/Kernels/SpriteStateKernel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework.Content;
+
+namespace WindowsGame1
+{
+    public abstract class SpriteStateKernel : ISpriteState
+    {
+        protected Counter Timer;
+        protected List<ISprite> SpriteList;
+
+        protected SpriteStateKernel(int frequency = 10)
+        {
+            Reset(frequency);
+        }
+
+        protected abstract void Initialize();
+
+        public abstract ISprite ActiveSprite();
+
+        public void Load(ContentManager content)
+        {
+            foreach (var sprite in SpriteList)
+            {
+                sprite.Load(content);
+            }
+        }
+
+        public void Reset(int frequency = 10)
+        {
+            Timer = new Counter(frequency);
+            Initialize();
+        }
+
+        public void Update()
+        {
+            ActiveSprite().Update();
+        }
+
+    }
+}
