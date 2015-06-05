10b6b8254dae2e21c666d13b94c9697243f7e27a
BOWEN DAI <bowen-dai@outlook.com>
2015-06-05 00:55:07 +0000

    Added 6 files to Sprites

---
+++ WindowsGame1/WindowsGame1/Mario/Sprites/FacingLeftBMarioSprite.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace WindowsGame1
+{
+    public class FacingLeftBMarioSprite : SpriteKernel
+    {
+        protected override void Initialize()
+        {
+            // Source parameters
+            Vector2 startCoordinate = new Vector2(175,50);
+            Vector2 endCoordinate = new Vector2(203, 85);
+
+            // Animation parameters
+            const int period = 1;
+
+            Source = new SpriteSource(startCoordinate, endCoordinate);
+            Animation = new SpriteAnimation(null, period);
+        }
+
+        public override void Load(ContentManager content)
+        {
+            Source.Load(content, "Mario");
+        }
+    }
+}
