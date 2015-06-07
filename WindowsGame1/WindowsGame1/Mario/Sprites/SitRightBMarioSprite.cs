81b34da46e04e2a32f372ec65dca39c13525de2e
BOWEN DAI <bowen-dai@outlook.com>
2015-06-07 03:28:10 +0000

    Added 4 files to Sprites

---
+++ WindowsGame1/WindowsGame1/Mario/Sprites/SitRightBMarioSprite.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace WindowsGame1
+{
+    public class SitRightBMarioSprite : SpriteKernel
+    {
+        protected override void Initialize()
+        {
+            // Source parameters
+            Vector2 startCoordinate = new Vector2(385,50);
+            Vector2 endCoordinate = new Vector2(405, 85);
+
+            // Animation parameters
+            const int period = 1;
+
+            Source = new SpriteSource(startCoordinate, endCoordinate);
+            Animation = new SpriteAnimation(null, period);
+        }
+
+        public override void Load(ContentManager content)
+        {
+            Source.Load(content, "Mario");
+        }
+    }
+}
