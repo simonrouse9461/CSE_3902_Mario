d75da7d2bb1726245975afbc2cfa8962da11440c
Haden Kersting <haden.kersting@gmail.com>
2015-06-09 00:14:25 -0700

    Included added files in project

---
+++ WindowsGame1/WindowsGame1/Enemies/GoombaObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+namespace WindowsGame1
+{
+    public  class GoombaObject : ObjectKernelNew<GoombaSpriteState, GoombaMotionState>
+    {
+        public  GoombaObject(Vector2 location) : base(location) { }
+
+        protected override void Initialize(Vector2 location)
+        {
+            SpriteState = new GoombaSpriteState();
+            MotionState = new GoombaMotionState(location);
+        }
+
+        protected void Reset()
+        {
+
+        }
+    }
+}
