40a254f7b39ea57f0fe0536cce3f7da568a79662
Colin <dolan.arsenal@gmail.com>
2015-06-12 09:05:25 -0400

    Updated to new SpriteSource

--- WindowsGame1/WindowsGame1/Items/Sprites/1upSprite.cs
+++ WindowsGame1/WindowsGame1/Items/Sprites/1upSprite.cs
@@ -1,18 +1,24 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 
 namespace WindowsGame1
 {
-    public class _1upSprite : SpriteKernel
+    public class _1upSprite : SpriteKernelNew
     {
         protected override void Initialize()
         {
-            const int totalFrames = 1;
-            Vector2 startCoordinate = new Vector2(208, 31);
-            Vector2 endCoordinate = new Vector2(235, 54);
+            //const int totalFrames = 1;
+            //startCoordinate = new Vector2(208, 31);
+            //Vector2 endCoordinate = new Vector2(235, 54);
             const int period = 1;
 
-            Source = new SpriteSource(startCoordinate, endCoordinate, totalFrames);
+            //Source = new SpriteSource(startCoordinate, endCoordinate, totalFrames);
+            Source = new SpriteSourceNew(
+                new List<Rectangle>
+                {
+                    new Rectangle(260, 114, 18, 18)
+                });
             Animation = new PeriodicFunction(
                 phase =>
                 {
