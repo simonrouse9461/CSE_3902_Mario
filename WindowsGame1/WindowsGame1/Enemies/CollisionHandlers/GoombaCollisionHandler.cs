7b359b681f2c1a07958f9d5656ccb009bb73c45b
Haden Kersting <haden.kersting@gmail.com>
2015-06-15 18:28:28 -0700

    Beginning Goomba Collision Detection

---
+++ WindowsGame1/WindowsGame1/Enemies/CollisionHandlers/GoombaCollisionHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace WindowsGame1
+{
+    public class GoombaCollisionHandler : CollisionHandlerKernel
+    {
+        private readonly GoombaSpriteState _spriteState;
+        private readonly GoombaMotionState _motionState;
+        
+        public GoombaCollisionHandler(GoombaSpriteState spriteState, GoombaMotionState motionState)
+        {
+            _spriteState = spriteState;
+            _motionState = motionState;
+        }
+
+        protected override void Initialize()
+        {
+            
+        }
+
+        public override void Handle()
+        {
+            
+        }
+    }
+}
