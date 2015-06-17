0f6019a25d78656b9be6a3c450cc9dcd0fc976fe
BOWEN DAI <bowen-dai@outlook.com>
2015-06-17 02:13:53 +0000

    Updated MarioCollisionHandler.cs

--- WindowsGame1/WindowsGame1/Mario/MarioCollisionHandler.cs
+++ WindowsGame1/WindowsGame1/Mario/MarioCollisionHandler.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace WindowsGame1
@@ -6,12 +6,16 @@ namespace WindowsGame1
     public class MarioCollisionHandler : CollisionHandlerKernel<MarioSpriteState, MarioMotionState>
     {
         private CollisionDetector<GreenPipeObject> MarioPipeCollision;
+        private CollisionDetector<Fireflower> MarioFireflowerCollision;
+        private CollisionDetector<Mushroom> MarioMushroomCollision;
 
         public MarioCollisionHandler(MarioSpriteState spriteState, MarioMotionState motionState, IObject obj) : base(spriteState, motionState, obj) { }
 
         protected override void Initialize()
         {
             MarioPipeCollision = new CollisionDetector<GreenPipeObject>(Object);
+            MarioFireflowerCollision = new CollisionDetector<Fireflower>(Object);
+            MarioMushroomCollision = new CollisionDetector<Mushroom>(Object);
         }
 
         public override void Handle()
@@ -20,6 +24,17 @@ namespace WindowsGame1
             {
                 SpriteState.BecomeDead();
                 MotionState.Dead();
+            }
+            if (MarioFireflowerCollision.Detect().Side())
+            {
+                SpriteState.BecomeFire();
+            }
+            if (MarioMushroomCollision.Detect().Side())
+            {
+                if (SpriteState.IsSmall())
+                {
+                    SpriteState.BecomeBig();
+                }
             }
         }
     }
