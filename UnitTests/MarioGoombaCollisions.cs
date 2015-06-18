a9a9bc1cf82d73ea1ee12de04a05d920d7742f87
Colin <dolan.arsenal@gmail.com>
2015-06-18 19:18:18 -0400

    Added Enemy and Block unit tests

---
+++ UnitTests/MarioGoombaCollisions.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using WindowsGame1;
+
+namespace MarioGoombaCollisions
+{
+    [TestClass]
+    public class MarioGoombaCollisions
+    {
+        private WorldManager TestWorld;
+        private MarioGame TestGame;
+        private MarioObject TestMario;
+        private Goomba TestGoomba;
+        private ContentManager Content;
+        private ICommand TestCommand;
+        [TestMethod]
+        public void MarioGoombaRightCollision()
+        {
+            TestGame = new MarioGame();
+            TestWorld = TestGame.World;
+            Content = TestGame.Content;
+            TestMario = TestGame.World.Mario;
+            TestGoomba = TestGame.World.Goomba;
+            TestCommand = new MarioRightCommand(TestGame);
+            TestMario.PassCommand(TestCommand);
+
+            Assert.AreEqual(true, TestMario.SpriteState.IsDead());
+        }
+
+        public void MarioGoombaLeftCollision()
+        {
+            TestGame = new MarioGame();
+            TestWorld = TestGame.World;
+            Content = TestGame.Content;
+            TestMario = TestGame.World.Mario;
+            TestGoomba = TestGame.World.Goomba;
+            TestCommand = new MarioLeftCommand(TestGame);
+            TestMario.PassCommand(TestCommand);
+
+            Assert.AreEqual(true, TestMario.SpriteState.IsDead());
+        }
+
+        public void MarioGoombaTopCollision()
+        {
+            TestGame = new MarioGame();
+            TestWorld = TestGame.World;
+            Content = TestGame.Content;
+            TestMario = TestGame.World.Mario;
+            TestGoomba = TestGame.World.Goomba;
+            TestCommand = new MarioDownCommand(TestGame);
+            TestMario.PassCommand(TestCommand);
+
+            Assert.AreEqual(false, TestMario.SpriteState.IsDead());
+        }
+        public void MarioGoombaBottomCollision()
+        {
+            TestGame = new MarioGame();
+            TestWorld = TestGame.World;
+            Content = TestGame.Content;
+            TestMario = TestGame.World.Mario;
+            TestGoomba = TestGame.World.Goomba;
+            TestCommand = new MarioUpCommand(TestGame);
+            TestMario.PassCommand(TestCommand);
+
+            Assert.AreEqual(true, TestMario.SpriteState.IsDead());
+        }
+    }
+}
+
