54ceca32baba30046b8c0949f7b6e72a58814a66
Andrew Miller <amiller956@gmail.com>
2015-06-18 20:23:08 -0400

    Conflicts

--- WindowsGame1/WindowsGame1/Mario/Sprites/JumpingLeftSmallMarioSprite.cs
+++ WindowsGame1/WindowsGame1/Mario/Sprites/JumpingLeftSmallMarioSprite.cs
@@ -9,25 +9,11 @@ namespace WindowsGame1
     {
         protected override void Initialize()
         {
-            const int period = 6;
-
             Source = new SpriteSource(
                 new Collection<Rectangle>
                 {
-                    new Rectangle(30, 30, 14, 16),
-                    new Rectangle(61, 30, 13 ,15),
-                    new Rectangle(90, 30, 14, 15),
-                    new Rectangle(120, 30, 14, 15),
-                    new Rectangle(150, 30, 14, 15),
-                    new Rectangle(180, 30, 15, 15)
+                    new Rectangle(29, 0, 17, 16)
                 });
-            Animation = new PeriodicFunction<int>(
-                phase =>
-                {
-                    int[] frameSequence = {5, 4, 3, 2, 1, 0};
-                    return frameSequence[phase];
-                }, 
-                period);
         }
 
         public override void Load(ContentManager content)
