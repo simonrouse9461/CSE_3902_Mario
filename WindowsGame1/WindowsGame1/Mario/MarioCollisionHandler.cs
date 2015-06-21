b62d0044e81815127d2e0c5477c7e4c7d1f8ad24
Chuhan Feng <simonrouse9461@gmail.com>
2015-06-21 19:00:44 -0400

    update

--- WindowsGame1/WindowsGame1/Mario/MarioCollisionHandler.cs
+++ WindowsGame1/WindowsGame1/Mario/MarioCollisionHandler.cs
@@ -14,22 +14,22 @@ namespace WindowsGame1
             if (SpriteState.IsDead())
                 return;
 
-            if (CollisionDetector.Detect<Fireflower>().Any())
+            if (CollisionDetector.Detect<Fireflower>().AnyEdge.Contact)
             {
                 SpriteState.BecomeFire();
             }
-            if (CollisionDetector.Detect<Mushroom>().Any())
+            if (CollisionDetector.Detect<Mushroom>().AnyEdge.Contact)
             {
                 if (SpriteState.IsSmall())
                 {
                     SpriteState.BecomeBig();
                 }
             }
-            if (CollisionDetector.Detect<Star>().Any())
+            if (CollisionDetector.Detect<Star>().AnyEdge.Contact)
             {
                 SpriteState.SetStarPower(true);
             }
-            if (CollisionDetector.Detect<Goomba>(goomba => goomba.Solid && goomba.Solid).Side())
+            if (CollisionDetector.Detect<Goomba>(goomba => goomba.Solid /*&& goomba.Alive*/).AnySide.Contact)
             {
                 if (SpriteState.IsSmall())
                 {
@@ -41,7 +41,7 @@ namespace WindowsGame1
                     SpriteState.BecomeSmall();
                 }
             }
-            if (CollisionDetector.Detect<Koopa>(koopa => koopa.Solid && koopa.Solid).Side())
+            if (CollisionDetector.Detect<Koopa>(koopa => koopa.Solid /*&& koopa.Alive*/).AnySide.Contact)
             {
                 if (SpriteState.IsSmall())
                 {
