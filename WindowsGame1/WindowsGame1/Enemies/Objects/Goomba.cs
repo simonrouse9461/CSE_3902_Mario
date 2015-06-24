3af2c1912f216b6e575aa263e539f881b5b6a2de
BOWEN DAI <bowen-dai@outlook.com>
2015-06-24 02:19:28 +0000

    Updated Goomba.cs

--- WindowsGame1/WindowsGame1/Enemies/Objects/Goomba.cs
+++ WindowsGame1/WindowsGame1/Enemies/Objects/Goomba.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 namespace WindowsGame1
 {
@@ -14,6 +14,11 @@ namespace WindowsGame1
         protected override void SyncState()
         {
 
+        }
+        
+        public bool Alive
+        {
+            get { return !SpriteState.Dead; }
         }
     }
 }
