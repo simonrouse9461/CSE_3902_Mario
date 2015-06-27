f2a7a3707b5434606b1b4a7b1878600fb3e03537
Chuhan Feng <simonrouse9461@gmail.com>
2015-06-27 19:45:59 -0400

    add IBarrierDetector

---
+++ WindowsGame1/WindowsGame1/Interfaces/IBarrierDetector.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WindowsGame1
+{
+    public interface IBarrierDetector
+    {
+        void AddBarrier<T>(Type type = null) where T : IObject;
+        void RemoveBarrier<T>(Type type = null) where T : IObject;
+        void Detect(); 
+    }
+}
