d3c445673a19009b04fbecd2a0d2677fd338f0bb
Andrew Miller <amiller956@gmail.com>
2015-06-30 12:11:12 -0400

    Added Fireball sprites

---
+++ WindowsGame1/WindowsGame1/Misc Objects/Sprites/ExplodingFireballSprite.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace WindowsGame1
+{
+    public class ExplodingFireballSprite : SpriteKernel
+    {
+        public ExplodingFireballSprite()
+        {
+            ImageFile.Default = "misc_sprites";
+            Source.Default = new SpriteSource
+            {
+                Coordinates = new Collection<Rectangle>
+                {
+                    new Rectangle(360, 946, 8, 8),
+                    new Rectangle(372, 943, 12, 14),
+                    new Rectangle(388, 942, 16, 16)
+                }
+            };
+        }
+    }
+}
