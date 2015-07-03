03a6753dc305228bae58602323de0a5140e80560
Chuhan Feng <simonrouse9461@gmail.com>
2015-07-02 21:56:53 -0400

    update

--- WindowsGame1/WindowsGame1/Misc Objects/Objects/FloorBlockObject.cs
+++ WindowsGame1/WindowsGame1/Misc Objects/Objects/FloorBlockObject.cs
@@ -3,7 +3,7 @@ using Microsoft.Xna.Framework;
 
 namespace WindowsGame1
 {
-    public class FloorBlockObject : ObjectKernel<BlockSpriteState, StaticMotionState>
+    public class FloorBlockObject : ObjectKernel<BlockSpriteState, StaticMotionState>, IBlock
     {
         public FloorBlockObject() {
             SpriteState.FloorBlock();
