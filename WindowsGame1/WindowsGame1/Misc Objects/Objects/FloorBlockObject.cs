10574709ae41f1a27be976edec0a58bd411d96f3
Chuhan Feng <simonrouse9461@gmail.com>
2015-07-02 21:45:56 -0400

    fix performance

--- WindowsGame1/WindowsGame1/Misc Objects/Objects/FloorBlockObject.cs
+++ WindowsGame1/WindowsGame1/Misc Objects/Objects/FloorBlockObject.cs
@@ -3,7 +3,7 @@ using Microsoft.Xna.Framework;
 
 namespace WindowsGame1
 {
-    public class FloorBlockObject : BlockKernel
+    public class FloorBlockObject : ObjectKernel<BlockSpriteState, StaticMotionState>
     {
         public FloorBlockObject() {
             SpriteState.FloorBlock();
