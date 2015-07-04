9d3ba1e9f1e33a9fc8a36871e6caf0f3d85936e3
Andrew Miller <amiller956@gmail.com>
2015-07-04 17:25:07 -0400

    Got rid of unneccessary used block classes and references

--- WindowsGame1/WindowsGame1/Misc Objects/Objects/UsedBlockObject.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections.Generic;
-using Microsoft.Xna.Framework;
-
-namespace WindowsGame1
-{
-    public class UsedBlockObject : ObjectKernel<UsedBlockSpriteState, StaticMotionState>, IBlock{}
-}
