d07f41d5405800b5ecbdedced59185cbdc97d327
Andrew Miller <amiller956@gmail.com>
2015-07-07 19:48:50 -0400

    Fireball updates

---
+++ WindowsGame1/WindowsGame1/Misc Objects/FireballStateController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsGame1
+{
+    public class FireballStateController : StateControllerKernel<FireballSpriteState, FireballMotionState>
+    {
+
+        private Collision collision;
+
+        public void Bounce()
+        {
+            if (collision.Top.Touch)
+            {
+                MotionState.Bounce();
+            }
+        }
+
+        public void Explode()
+        {
+            MotionState.Stop();
+            SpriteState.Exploded();
+        }
+
+        public void HitObject()
+        {
+            if (collision.AnySide.Touch)
+            {
+                Explode();
+            }
+        }
+
+        protected override void UpdateState()
+        {
+            HitObject();
+            Bounce();
+        }
+    }
+}
