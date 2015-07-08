371bbe6d0674d34816d5250316b65182aaa2aa7f
BOWEN DAI <bowen-dai@outlook.com>
2015-07-08 02:58:43 +0000

    Updated GoombaCollisionHandler.cs

--- WindowsGame1/WindowsGame1/Enemies/CollisionHandlers/GoombaCollisionHandler.cs
+++ WindowsGame1/WindowsGame1/Enemies/CollisionHandlers/GoombaCollisionHandler.cs
@@ -11,7 +11,11 @@ namespace WindowsGame1
         public override void Handle()
         {
             if (!Core.StateController.SpriteState.Dead) {
-                if (Core.CollisionDetector.Detect<MarioObject>(mario => (mario.Alive && mario.GoingDown) || mario.StarPower).Top.Touch)
+               if (Core.CollisionDetector.Detect<MarioObject>(mario =>mario.StarPower).AnyEdge.Touch)
+                {
+                    Core.StateController.MarioSmash();
+                }
+                if (Core.CollisionDetector.Detect<MarioObject>(mario => (mario.Alive && mario.GoingDown) ).Top.Touch)
                 {
                     Core.StateController.MarioSmash();
                 }
