9ffb7f8aefc983cd504620954cb84b689e02c8eb
Andrew Miller <amiller956@gmail.com>
2015-07-09 19:12:29 -0400

    Add version to floor block object

--- WindowsGame1/WindowsGame1/Misc Objects/Objects/FloorBlockObject.cs
+++ WindowsGame1/WindowsGame1/Misc Objects/Objects/FloorBlockObject.cs
@@ -5,8 +5,26 @@ namespace WindowsGame1
 {
     public class FloorBlockObject : BlockKernel
     {
+
+        private enum Version
+        {
+            Invisible,
+            Default
+        }
+
+        private Version version = Version.Default;
+
         public FloorBlockObject() {
             StateController.FloorBlock();
+        }
+
+        public FloorBlockObject Invisible
+        {
+            get
+            {
+                version = Version.Invisible;
+                return this;
+            }
         }
     }
 }
