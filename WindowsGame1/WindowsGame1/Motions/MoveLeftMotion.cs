7b29720394f94aa7defb868dd86b906bf0fe581e
Chuhan Feng <simonrouse9461@gmail.com>
2015-07-21 16:05:23 -0400

    add file

---
+++ WindowsGame1/WindowsGame1/Motions/MoveLeftMotion.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace WindowsGame1
+{
+    public class MoveLeftMotion : MotionKernel
+    {
+        private enum Version
+        {
+            Default,
+            Enemy,
+            Item,
+            Fireball
+        }
+
+        private Version version = Version.Default;
+
+        public override int VersionCode
+        {
+            get { return (int)version; }
+        }
+
+        public override Vector2 Velocity
+        {
+            get
+            {
+                var velocity = StartVelocity;
+                return velocity;
+            }
+        }
+
+        // Versions
+
+        public static MoveLeftMotion EnemyVelocity
+        {
+            get
+            {
+                return new MoveLeftMotion
+                {
+                    version = Version.Enemy,
+                    StartVelocity = new Vector2(-1, 0)
+                };
+            }
+        }
+
+        public static MoveLeftMotion ItemVelocity
+        {
+            get
+            {
+                return new MoveLeftMotion
+                {
+                    version = Version.Item,
+                    StartVelocity = new Vector2(-1, 0)
+                };
+            }
+        }
+
+        public static MoveLeftMotion FireballVelocity
+        {
+            get
+            {
+                return new MoveLeftMotion
+                {
+                    version = Version.Fireball,
+                    StartVelocity = new Vector2(-6, 0)
+                };
+            }
+        }
+    }
+}
---
+++ WindowsGame1/WindowsGame1/Motions/MoveRightMotion.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace WindowsGame1
+{
+    public class MoveRightMotion : MotionKernel
+    {
+        private enum Version
+        {
+            Default,
+            Enemy,
+            Item,
+            Fireball
+        }
+
+        private Version version = Version.Default;
+
+        public override int VersionCode
+        {
+            get { return (int)version; }
+        }
+
+        public override Vector2 Velocity
+        {
+            get
+            {
+                var velocity = StartVelocity;
+                return velocity;
+            }
+        }
+
+        // Versions
+
+        public static MoveRightMotion EnemyVelocity
+        {
+            get
+            {
+                return new MoveRightMotion
+                {
+                    version = Version.Enemy,
+                    StartVelocity = new Vector2(1, 0)
+                };
+            }
+        }
+
+        public static MoveRightMotion ItemVelocity
+        {
+            get
+            {
+                return new MoveRightMotion
+                {
+                    version = Version.Item,
+                    StartVelocity = new Vector2(1, 0)
+                };
+            }
+        }
+
+        public static MoveRightMotion FireballVelocity
+        {
+            get
+            {
+                return new MoveRightMotion
+                {
+                    version = Version.Fireball,
+                    StartVelocity = new Vector2(6, 0)
+                };
+            }
+        }
+    }
+}
