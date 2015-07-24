f66e810728b4eb953fcbb14b45e6535f2bab8b86
Chuhan Feng <simonrouse9461@gmail.com>
2015-07-24 01:15:59 -0400

    update

--- LevelGenerator/Program.cs
+++ LevelGenerator/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
 using SuperMario;
@@ -10,9 +12,6 @@ namespace LevelGenerator
 {
     class Program
     {
-        public const string OutFile = "";
-        public const int GridUnit = 16;
-
         public struct Item
         {
             public string Type;
@@ -51,17 +50,18 @@ namespace LevelGenerator
         {
             InitializeData();
 
-            WriteHeader();
-
-            foreach (var item in Data)
+            using (var fout = new StreamWriter(GenerateHistoryPath()))
             {
-                foreach (var section in item.Value)
+                WriteHeader(fout);
+                foreach (var item in Data)
                 {
-                    WriteSection(item.Key, section);
+                    foreach (var section in item.Value)
+                    {
+                        WriteSection(fout, item.Key, section);
+                    }
                 }
+                WriteFooter(fout);
             }
-
-            WriteFooter();
         }
 
         public static void InitializeData()
@@ -75,31 +75,44 @@ namespace LevelGenerator
             };
         }
 
-        public static void WriteHeader()
+        public static string GenerateHistoryPath()
         {
-            Console.WriteLine("<?xml version=\"1.0\" encoding=\"utf-8\" ?>");
-            Console.WriteLine("<XnaContent>");
-            Console.WriteLine("  <Asset Type=\"LevelLoader.ObjectData[]\">");
+            var time = DateTime.Now;
+            var timeString = string.Format("{0:D4}{1:D2}{2:D2}{3:D2}{4:D2}{5:D2}",
+                time.Year, time.Month, time.Day, time.Hour, time.Minute, time.Second);
+            return @"..\..\History\LevelData" + timeString + ".xml";
         }
 
-        public static void WriteFooter()
+        public static string GetOutputPath(string fileName)
         {
-            Console.WriteLine("  </Asset>");
-            Console.WriteLine("</XnaContent>");
+            return @"..\..\..\";
         }
 
-        public static void WriteSection(Item item, Section section)
+        public static void WriteHeader(StreamWriter fout)
+        {
+            fout.WriteLine("<?xml version=\"1.0\" encoding=\"utf-8\" ?>");
+            fout.WriteLine("<XnaContent>");
+            fout.WriteLine("  <Asset Type=\"LevelLoader.ObjectData[]\">");
+        }
+
+        public static void WriteFooter(StreamWriter fout)
+        {
+            fout.WriteLine("  </Asset>");
+            fout.WriteLine("</XnaContent>");
+        }
+
+        public static void WriteSection(StreamWriter fout, Item item, Section section)
         {
             for (var column = 0; column < section.Column; column++)
                 for (var row = 0; row < section.Row; row++)
                 {
-                    var locationX = (int) ((column + 0.5)*GridUnit*GameSettings.SpriteScale*item.GridWidth);
-                    var locationY = (int) (Camera.Height - row*GridUnit*GameSettings.SpriteScale*item.GridHeight);
-                    Console.WriteLine("    <Item>");
-                    Console.WriteLine("      <Type>" + item.Type + "</Type>");
-                    Console.WriteLine("      <Version>" + item.Version + "</Version>");
-                    Console.WriteLine("      <Location>" + locationX + " " + locationY + "</Location>");
-                    Console.WriteLine("    </Item>");
+                    var locationX = (int) ((column + 0.5)*GameSettings.GridUnit*GameSettings.SpriteScale*item.GridWidth);
+                    var locationY = (int) (Camera.Height - row*GameSettings.GridUnit*GameSettings.SpriteScale*item.GridHeight);
+                    fout.WriteLine("    <Item>");
+                    fout.WriteLine("      <Type>" + item.Type + "</Type>");
+                    fout.WriteLine("      <Version>" + item.Version + "</Version>");
+                    fout.WriteLine("      <Location>" + locationX + " " + locationY + "</Location>");
+                    fout.WriteLine("    </Item>");
                 }
         }
     }
--- WindowsGame1/WindowsGame1/Utility Classes/GameSettings.cs
+++ WindowsGame1/WindowsGame1/Utility Classes/GameSettings.cs
@@ -6,26 +6,22 @@ namespace SuperMario
     {
         public static float SpriteScale
         {
-            get
-            {
-                return 1.75f;
-            }
+            get { return 1.75f; }
+        }
+
+        public static int GridUnit
+        {
+            get { return 16; }
         }
 
         public static int MaxTime
         {
-            get
-            {
-                return 120;
-            }
+            get { return 120; }
         }
 
         public static SamplerState TextureSampling
         {
-            get
-            {
-                return SamplerState.PointClamp;
-            }
+            get { return SamplerState.PointClamp; }
         }
     }
 }
