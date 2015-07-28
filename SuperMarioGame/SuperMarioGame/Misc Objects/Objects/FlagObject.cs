0b9226d3867017e446cc65835966ff1b1da77719
BOWEN DAI <bowen-dai@outlook.com>
2015-07-28 01:58:30 +0000

    Added 2 files to Objects

---
+++ SuperMarioGame/SuperMarioGame/Misc Objects/Objects/FlagObject.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace SuperMario
+{
+    public class FlagObject : ObjectKernelNew<FlagStateController>
+    {
+        public FlagObject()
+        {
+            StateController.Flag();
+        }
+
+        //public override Rectangle CollisionRectangle
+        //{
+        //    get
+        //    {
+        //        return new Rectangle(
+        //            (int)(PositionRectangle.X + 3*GameSettings.SpriteScale), 
+        //            PositionRectangle.Y, 
+        //            (int)(PositionRectangle.Width - 6*GameSettings.SpriteScale),
+        //            PositionRectangle.Height);
+        //    }
+        //}
+    }
+}
