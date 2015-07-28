e2dc5497ecb035685e91f9bac4d667287173afc2
BOWEN DAI <bowen-dai@outlook.com>
2015-07-28 01:59:28 +0000

    Added 2 files to Sprites

---
+++ SuperMarioGame/SuperMarioGame/Misc Objects/Sprites/FlagSprite.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace SuperMario
+{
+    public class FlagSprite : SpriteKernelNew
+    {
+        public FlagSprite() : base(FlagSpriteVersion.Overworld)
+        {
+
+            AddSource(
+                FlagSpriteVersion.Overworld,
+                "misc",
+                new OrderedPairs<Rectangle, Orientation>
+                {
+                    {new Rectangle(279,598,16,16), Orientation.Default},
+                });
+            
+        }    
+    }
+}
