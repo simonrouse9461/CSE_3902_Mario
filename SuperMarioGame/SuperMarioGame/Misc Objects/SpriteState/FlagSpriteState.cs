73a2ab05ff73141807dd54dbf591d96cf41b47cf
BOWEN DAI <bowen-dai@outlook.com>
2015-07-28 01:59:55 +0000

    Added 2 files to SpriteState

---
+++ SuperMarioGame/SuperMarioGame/Misc Objects/SpriteState/FlagSpriteState.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+using Microsoft.Xna.Framework;
+
+namespace SuperMario
+{
+    public class FlagSpriteState : SpriteStateKernelNew<FlagSpriteVersion>
+    {
+
+        public FlagSpriteState()
+        {
+            AddSprite<FlagSprite>();
+        }
+
+        public void SetFlag()
+        {
+            SetSprite<FlagSprite>();
+        }
+    }
+}
